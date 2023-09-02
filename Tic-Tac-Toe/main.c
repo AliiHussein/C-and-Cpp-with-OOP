dfc65ffe9fa84b5c8cc1ecb47df84b456e6dd72e
AliiHussein <aliunali@yahoo.com>
2023-09-02 17:14:02 +0300

    C version of the game

--- Tic-Tac-Toe/main.c
+++ Tic-Tac-Toe/main.c
@@ -1,26 +1,60 @@
 #include <stdio.h>
 
 // global variables 
-char board[3][3] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
-unsigned char current_maker;
-unsigned char current_player;
+unsigned char board[3][3] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
+unsigned char current_maker = 'X';
+unsigned int current_player = 1;
+unsigned int rounds = 0;
+char isWinner = 0;
 
 // Functions prototypes
 void draw_board(void);
+void change_players(void);
+void winner_check(void);
 
 int main(){
+
+    // Game initialization
     int number;
+    draw_board();
 
-    printf("%c", board[0][4]);
+    while (1)
+    {
+        printf("Player %d turn\n", current_player);
+        printf("Enter a number: ");
+        scanf("%d", &number);
 
-    // while (1)
-    // {
-    //     printf("Player 1 turn\n");
-    //     printf("Enter a number: ");
-    //     scanf("%d", &number);
-    //     board[number]= 'x';
-    //     draw_board();
-    // }
+        if(number < 1 || number > 9){
+            printf("Invalid number/place, out of bounds\n");
+            continue;
+        }
+
+        if((board[0][number-1] == 'X') || (board[0][number-1] == 'O')){
+            printf("Place already taken, choose another place\n");
+            continue;
+        }
+
+        
+        board[0][number-1]= current_maker;
+        printf("\n");
+        printf("\n");
+        draw_board();
+        rounds++;
+
+
+        winner_check();
+
+        if(isWinner){
+            break;
+        }
+
+        if(rounds == 9){
+            printf("DRAW");
+            break;
+        }
+
+        change_players();
+    }
     
     return 0;
 }
@@ -34,4 +68,60 @@ void draw_board(){
         }
         printf("\n");
     }
+}
+
+void change_players(){
+    if(current_maker == 'X'){
+        current_maker = 'O';
+        current_player = 2;
+    }
+    else{
+        current_maker = 'X';
+        current_player = 1;
+    }
+}
+
+void winner_check(){
+    if(rounds < 5){
+        return;
+    }
+
+    // Rows
+    if((board[0][0] == board[0][1]) &&(board[0][1] == board[0][2])){
+        printf("Player %d is the winner\n", current_player);
+        isWinner = 1;
+    }
+    else if((board[1][0] == board[1][1]) &&(board[1][1] == board[1][2])){
+        printf("Player %d is the winner\n", current_player);
+        isWinner = 1;
+    }
+    else if((board[2][0] == board[2][1]) &&(board[2][1] == board[2][2])){
+        printf("Player %d is the winner\n", current_player);
+        isWinner = 1;
+    }
+
+    // columns
+    else if((board[0][0] == board[1][0]) &&(board[1][0] == board[2][0])){
+        printf("Player %d is the winner\n", current_player);
+        isWinner = 1;
+    }
+    else if((board[0][1] == board[1][1]) &&(board[1][1] == board[2][1])){
+        printf("Player %d is the winner\n", current_player);
+        isWinner = 1;
+    }
+    else if((board[0][2] == board[1][2]) &&(board[1][2] == board[2][2])){
+        printf("Player %d is the winner\n", current_player);
+        isWinner = 1;
+    }
+
+    // Diagonals
+    else if((board[0][0] == board[1][1]) &&(board[1][1] == board[2][2])){
+        printf("Player %d is the winner\n", current_player);
+        isWinner = 1;
+    }
+    else if((board[0][2] == board[1][1]) &&(board[1][1] == board[2][0])){
+        printf("Player %d is the winner\n", current_player);
+        isWinner = 1;
+    } 
+
 }
