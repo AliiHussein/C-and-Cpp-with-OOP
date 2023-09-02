adc21ba98bfe5a3145d5a25c340e3040cec5cc43
AliiHussein <aliunali@yahoo.com>
2023-09-02 16:44:15 +0300

    adding checks

--- Tic-Tac-Toe/main.cpp
+++ Tic-Tac-Toe/main.cpp
@@ -5,9 +5,12 @@ using namespace std;
 unsigned char board[3][3] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
 unsigned char current_maker = 'X';
 unsigned int current_player = 1;
+unsigned int rounds = 0;
 
 // Functions prototypes
 void draw_board(void);
+void change_players(void);
+void game_check(void);
 
 int main(){
     int number;
@@ -15,18 +18,38 @@ int main(){
     while (1)
     {
         cout << "Player " << current_player << " turn" << endl;
+
         draw_board();
+
         cout << "Enter a number: ";
         cin >> number;
+
+        cout << endl;
+        cout << endl;
+
+        if(number < 1 || number > 9){
+            cout << "Invalid number/place, out of bounds" << endl;
+            continue;
+        }
+
+        if((board[0][number-1] == 'X') || (board[0][number-1] == 'O')){
+            cout << "Place already taken, choose another place" << endl;
+            continue;
+        }
+
+        
         board[0][number-1]= current_maker;
-        if(current_maker == 'X'){
-            current_maker = 'O';
-            current_player = 2;
+        rounds++;
+
+        if(rounds == 9){
+            cout << "DRAW" << endl;
+            break;
         }
-        else{
-            current_maker = 'X';
-            current_player = 1;
-        }
+
+        game_check();
+        
+
+        change_players();
     }
     
     return 0;
@@ -41,4 +64,22 @@ void draw_board(){
         }
         cout << endl;
     }
+}
+
+void change_players(){
+    if(current_maker == 'X'){
+        current_maker = 'O';
+        current_player = 2;
+    }
+    else{
+        current_maker = 'X';
+        current_player = 1;
+    }
+}
+
+void game_check(){
+    if(rounds < 5){
+        return;
+    }
+    cout << "I entered" << endl;
 }
