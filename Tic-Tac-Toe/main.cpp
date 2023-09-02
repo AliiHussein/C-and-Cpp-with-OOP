d90b6bca869b83c74021b5a4b6197dba2f136f4c
AliiHussein <aliunali@yahoo.com>
2023-09-02 17:06:14 +0300

    adding the game end

--- Tic-Tac-Toe/main.cpp
+++ Tic-Tac-Toe/main.cpp
@@ -6,26 +6,26 @@ unsigned char board[3][3] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} 
 unsigned char current_maker = 'X';
 unsigned int current_player = 1;
 unsigned int rounds = 0;
+bool isWinner = false;
 
 // Functions prototypes
 void draw_board(void);
 void change_players(void);
-void game_check(void);
+void winner_check(void);
 
 int main(){
+
+    // Game initialization
     int number;
+    draw_board();
 
     while (1)
     {
         cout << "Player " << current_player << " turn" << endl;
 
-        draw_board();
-
         cout << "Enter a number: ";
         cin >> number;
 
-        cout << endl;
-        cout << endl;
 
         if(number < 1 || number > 9){
             cout << "Invalid number/place, out of bounds" << endl;
@@ -39,15 +39,22 @@ int main(){
 
         
         board[0][number-1]= current_maker;
+        cout << endl;
+        cout << endl;
+        draw_board();
         rounds++;
+
+
+        winner_check();
+
+        if(isWinner){
+            break;
+        }
 
         if(rounds == 9){
             cout << "DRAW" << endl;
             break;
         }
-
-        game_check();
-        
 
         change_players();
     }
@@ -77,9 +84,47 @@ void change_players(){
     }
 }
 
-void game_check(){
+void winner_check(){
     if(rounds < 5){
         return;
     }
-    cout << "I entered" << endl;
+
+    // Rows
+    if((board[0][0] == board[0][1]) &&(board[0][1] == board[0][2])){
+        cout << "Player "<< current_player << " is the winner" << endl;
+        isWinner = true;
+    }
+    else if((board[1][0] == board[1][1]) &&(board[1][1] == board[1][2])){
+        cout << "Player "<< current_player << " is the winner" << endl;
+        isWinner = true;
+    }
+    else if((board[2][0] == board[2][1]) &&(board[2][1] == board[2][2])){
+        cout << "Player "<< current_player << " is the winner" << endl;
+        isWinner = true;
+    }
+
+    // columns
+    else if((board[0][0] == board[1][0]) &&(board[1][0] == board[2][0])){
+        cout << "Player "<< current_player << " is the winner" << endl;
+        isWinner = true;
+    }
+    else if((board[0][1] == board[1][1]) &&(board[1][1] == board[2][1])){
+        cout << "Player "<< current_player << " is the winner" << endl;
+        isWinner = true;
+    }
+    else if((board[0][2] == board[1][2]) &&(board[1][2] == board[2][2])){
+        cout << "Player "<< current_player << " is the winner" << endl;
+        isWinner = true;
+    }
+
+    // Diagonals
+    else if((board[0][0] == board[1][1]) &&(board[1][1] == board[2][2])){
+        cout << "Player "<< current_player << " is the winner" << endl;
+        isWinner = true;
+    }
+    else if((board[0][2] == board[1][1]) &&(board[1][1] == board[2][0])){
+        cout << "Player "<< current_player << " is the winner" << endl;
+        isWinner = true;
+    } 
+
 }
