5292bd7567683c6809afad0ed57fb17ddbf704b4
AliiHussein <aliunali@yahoo.com>
2023-09-08 01:11:35 +0300

    reversing a hexadecimal using pointers

---
+++ C_with_SA/Bitwise/ReversingHexadecimal_Pointers.c
@@ -0,0 +1,26 @@
+/*
+Reversing a hexadecimal number using two method
+
+- Bitwise operations
+- pointers
+    - using shift
+    - using swap
+
+*/
+#include <stdio.h>
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+
+void main(){
+	uint16 num;
+	uint16 reversed;
+	scanf("%x", &num);
+
+	uint8 *p = &num;
+
+	reversed = *(p+1);
+
+
+	printf("Reversed: %x\n", reversed);
+}
