dc49f3e1a6d737bcaa5294b07f310d7f165f5cc7
AliiHussein <aliunali@yahoo.com>
2023-10-02 21:12:49 +0300

    Future work strncpy

--- C Libraries/string/README.md
+++ C Libraries/string/README.md
@@ -6,3 +6,4 @@ link: https://www.geeksforgeeks.org/c-library-string-h/
 ## Libraries implemented
 1. strlen() -> It returns the length of the string
 2. strcpy() -> Copy one string to another.
+3. strncpy() -> Copy first n characters of one string to another.
--- C Libraries/string/main.c
+++ C Libraries/string/main.c
@@ -2,10 +2,19 @@
 #include <string.h>
 #include "myString.h"
 
+
 void main(){
     char s[20];
     char s2[20];
     scanf("%[^\n]%*c", s);
 
-    printf("%d, %d", my_strlen(s), strlen(s));
+    strcpy(s2, s);
+
+    printf("Original Strcpy: s: %s    s2: %s \n",s,s2);
+
+    scanf("%[^\n]%*c", s);
+
+    my_strcpy(s2, s);
+
+    printf("My Strcpy: s: %s    s2: %s \n",s,s2);
 }
